43d568f04aef5f29c20d0732f951b34d5b4aff2d
Herlan Wibowo <herlanwibowo18@gmail.com>
2024-01-19 22:30:40 +0700

    UAS

---
+++ app/src/main/java/id/co/myapplication/MyName.java
@@ -0,0 +1,67 @@
+package id.co.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MyName extends Fragment {
+
+    private ImageView MyName;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_action, container, false);
+
+        MyName = view.findViewById(R.id.myname);
+
+        MyName.setOnClickListener(v -> playVideo("myname"));
+
+        return view;
+    }
+
+    private void playVideo(String videoFileName) {
+        String videoPath = "android.resource://" + requireActivity().getPackageName() + "/raw/" + videoFileName;
+        Uri videoUri = Uri.parse(videoPath);
+
+        Intent intent = new Intent(requireContext(), VideoActivity.class);
+        intent.putExtra("VIDEO_URI", videoUri.toString());
+        startActivity(intent);
+    }
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public MyName() {
+        // Konstruktor publik kosong yang diperlukan
+    }
+
+    public static MyName newInstance(String param1, String param2) {
+        MyName fragment = new MyName();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+}
